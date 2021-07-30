c25ec6b9919532b455533d828c47b1e2ecda841d
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2021-07-30 21:57:00 +0200

    Initialize breaking

---
+++ src/App/index.js
@@ -0,0 +1,29 @@
+import Main from './Main';
+
+const {
+  libraries: {
+    React,
+    ReactRedux: { useSelector },
+    emotion: {
+      createCache,
+      core: { CacheProvider },
+    },
+  },
+  components: { ThemeController },
+} = NEXUS;
+
+const emotionCache = createCache({ container: document.head, key: 'emotion' });
+
+export default function App() {
+  const initialized = useSelector((state) => state.initialized);
+  const theme = useSelector((state) => state.theme);
+  if (!initialized) return null;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeController theme={theme}>
+        <Main />
+      </ThemeController>
+    </CacheProvider>
+  );
+}
